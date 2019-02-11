26c4a24fd45b60f899b4b5f563bb3ac1b7a40ef1
vilson junior <vilsonrjunior@gmail.com>
2019-02-11 14:06:31 +0800

    added validations

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates_uniqueness_of :ingredient, :scope => [:cocktail]
+
+
+  validates :description, presence: true
+end
